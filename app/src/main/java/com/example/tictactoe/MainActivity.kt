70665bdad343dfc9f2f57d44f75e57609c260a51
Khussan Aminzhanov <aminjanovhusan07@gmail.com>
2022-05-30 16:38:47 +0600

    second commit

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,15 +1,51 @@
 package com.example.tictactoe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val model: MainViewModel by lazy { ViewModelProvider(this)[MainViewModel::class.java] }
+    private lateinit var model: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
+    private var isCrossesMove = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        model = ViewModelProvider(this)[MainViewModel::class.java]
+        setContentView(binding.root)
+        addCells()
+    }
+
+    private fun addCells() {
+        repeat(9) { binding.gridLayout.addView(getCell(it)) }
+    }
+
+    private fun getCell(id: Int): Button {
+        val cell = layoutInflater.inflate(R.layout.btn_cell, null) as Button
+        cell.setOnClickListener {
+            model.mark(id, isCrossesMove)
+            if (isCrossesMove) {
+                changeBackground(cell, R.drawable.btn_cell_crosses, "X")
+            } else {
+                changeBackground(cell, R.drawable.btn_cell_noughts, "O")
+            }
+            cell.isClickable = false
+            if (model.hasWon(isCrossesMove)) Toast.makeText(this, "Won!", Toast.LENGTH_LONG).show()
+            isCrossesMove = !isCrossesMove
+        }
+        return cell
+    }
+
+    private fun changeBackground(button: Button, drawable: Int, text: String) {
+        button.background = AppCompatResources.getDrawable(this, drawable)
+        button.text = text
     }
 }
--- app/src/main/java/com/example/tictactoe/MainViewModel.kt
+++ app/src/main/java/com/example/tictactoe/MainViewModel.kt
@@ -4,4 +4,64 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
+    private val field = Array<Boolean?>(9) { null }
+
+    fun mark(index: Int, isCross: Boolean) {
+        field[index] = isCross
+    }
+
+    fun hasWon(isCross: Boolean): Boolean {
+        val length = 3
+        return checkRows(isCross, length, length - 1)
+    }
+
+    private fun checkDiagonal(isCross: Boolean, length: Int): Boolean {
+        val indices = getDiagonal(0, length * length, length + 1)
+        for (i in indices) if (field[i] != isCross) return false
+        return true
+    }
+
+    private fun checkReverseDiagonal(isCross: Boolean, length: Int): Boolean {
+        val indices = getDiagonal(length - 1, (length - 1) * length + 1, length - 1)
+        for (i in indices) if (field[i] != isCross) return false
+        return true
+    }
+
+    private fun checkRow(isCross: Boolean, length: Int, row: Int): Boolean {
+        val indices = getRow(row, length)
+        for (i in indices) if (field[i] != isCross) return false
+        return true
+    }
+
+    private fun checkRows(isCross: Boolean, length: Int, row: Int): Boolean {
+        if (row == 0) return checkRow(isCross, length, row)
+        return checkRows(isCross, length, row - 1) || checkRow(isCross, length, row)
+    }
+
+    private fun getDiagonal(start: Int, end: Int, step: Int): List<Int> {
+        val list = mutableListOf<Int>()
+        for (i in start until end step step) list.add(i)
+        return list
+    }
+
+    private fun getRow(row: Int, length: Int): List<Int> {
+        val list = mutableListOf<Int>()
+        repeat(length) { print("${(row * length) + it}\t") }
+        return list
+    }
+
+    private fun getColumn(column: Int, length: Int): List<Int> {
+        val list = mutableListOf<Int>()
+        repeat(length) { print("${it * length + column}\t") }
+        return list
+    }
+
+    private fun printField() {
+        val text = ""
+        repeat(3) {
+            repeat(3) {
+
+            }
+        }
+    }
 }
